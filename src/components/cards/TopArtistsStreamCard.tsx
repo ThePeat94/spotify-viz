a1c6039db45f562baef21505f15fcfb9c70bb27e
Peat <hendy123@gmx.de>
2025-04-24 23:06:00 +0200

    add filter for top artists

--- src/components/cards/TopArtistsStreamCard.tsx
+++ src/components/cards/TopArtistsStreamCard.tsx
@@ -1,11 +1,12 @@
 import React, { useMemo, useState } from 'react';
 import {
+    Autocomplete,
     Box,
     Card,
     CardContent,
     CardHeader,
     ListItem,
-    ListItemText
+    ListItemText, Stack, TextField
 } from '@mui/material';
 import { ArtistStatsType } from 'src/stats/type';
 import { SortModeSelect, SortModeType } from 'src/components/SortModeSelect';
@@ -16,9 +17,13 @@ type TopArtistsStreamCardProps = {
     artistStats: ArtistStatsType[];
 };
 
+type RankedArtistStatsType = ArtistStatsType & {
+    rank: number;
+}
+
 const DEFAULT_LIST_ITEM_SIZE = 75;
 
-const TopArtistListItem : React.FC<ListChildComponentProps<ArtistStatsType[]>> = (props: ListChildComponentProps<ArtistStatsType[]>) => {
+const TopArtistListItem : React.FC<ListChildComponentProps<RankedArtistStatsType[]>> = (props: ListChildComponentProps<RankedArtistStatsType[]>) => {
     const { index, style, data } = props;
 
     const  artist  = data[index];
@@ -26,7 +31,7 @@ const TopArtistListItem : React.FC<ListChildComponentProps<ArtistStatsType[]>> =
     return (
         <ListItem style={style} key={index} disablePadding={true}>
             <ListItemText
-                primary={<>#{index + 1} - {artist.name}</>}
+                primary={<>#{artist.rank} - {artist.name}</>}
                 secondary={<>{artist.count} streams - <HoverableDuration durationInMs={artist.msPlayed} decimalNumbers={0}/>  - First Stream: {artist.firstStream.format('DD.MM.YYYY HH:mm')} - Last Stream: {artist.lastStream.format('DD.MM.YYYY HH:mm')}</>}
             />
         </ListItem>
@@ -43,10 +48,32 @@ const TopArtistsStreamCard: React.FC<TopArtistsStreamCardProps> = (props) => {
 
     const [sortMode, setSortMode] = useState<SortModeType>('count');
 
+    const [selectedArtist, setSelectedArtist] = useState<string>();
 
-    const sortedPerArtist = useMemo(() => {
-        return artistStats.sort((p1, p2) => p2[sortMode] - p1[sortMode]);
-    }, [artistStats, sortMode]);
+    const artistOptions = useMemo(() => {
+        return artistStats.map(artist => ({
+            label: artist.name,
+        })).sort((a, b) => a.label.localeCompare(b.label));
+    }, [artistStats]);
+
+
+    const sortedPerArtist: RankedArtistStatsType[]  = useMemo(() => {
+        const transformed = artistStats.sort((p1, p2) => p2[sortMode] - p1[sortMode]).map((artist, index) => ({
+            ...artist,
+            rank: index + 1,
+        }));
+
+        if (selectedArtist) {
+            console.log('Selected artist: ', selectedArtist);
+            return transformed.filter(artist => artist.name === selectedArtist);
+        }
+
+        return transformed;
+    }, [artistStats, sortMode, selectedArtist]);
+
+    const handleSelectedArtistChange = (artist?: string): void => {
+        setSelectedArtist(artist);
+    };
 
     return (
         <Card>
@@ -62,16 +89,25 @@ const TopArtistsStreamCard: React.FC<TopArtistsStreamCardProps> = (props) => {
                 }
             />
             <CardContent>
-                <VariableSizeList
-                    itemSize={() => DEFAULT_LIST_ITEM_SIZE}
-                    height={Math.min(sortedPerArtist.length * DEFAULT_LIST_ITEM_SIZE, 500)}
-                    itemData={sortedPerArtist}
-                    itemCount={sortedPerArtist.length}
-                    width={'100%'}
-                    overscanCount={10}
-                >
-                    {TopArtistListItem}
-                </VariableSizeList>
+                <Stack spacing={2}>
+                    <Autocomplete
+                        disablePortal={true}
+                        options={artistOptions}
+                        fullWidth={true}
+                        renderInput={(params) => <TextField {...params} label={'Artist'} />}
+                        onChange={(_, newValue) => handleSelectedArtistChange(newValue?.label)}
+                    />
+                    <VariableSizeList
+                        itemSize={() => DEFAULT_LIST_ITEM_SIZE}
+                        height={500}
+                        itemData={sortedPerArtist}
+                        itemCount={sortedPerArtist.length}
+                        width={'100%'}
+                        overscanCount={10}
+                    >
+                        {TopArtistListItem}
+                    </VariableSizeList>
+                </Stack>
             </CardContent>
         </Card>
     );
