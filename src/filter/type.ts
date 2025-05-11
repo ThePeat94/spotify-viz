683e776c3f6e5369abe25bf2206fc80c8754ef95
Peat <hendy123@gmx.de>
2025-05-11 23:30:14 +0200

    move stuff

--- src/filter/type.ts
+++
@@ -1,5 +0,0 @@
-export type DataFilterType = {
-    minDuration: number;
-    from?: number;
-    to?: number;
-}
--- src/utils/performance.ts
+++
@@ -1,7 +0,0 @@
-export const performAndMeasure = <T>(name: string, action: () => T): T  => {
-    const start = performance.now();
-    const result = action();
-    const end = performance.now();
-    console.log(`Execution time for ${name}: ${end - start} ms`);
-    return result;
-};
