e716c70f69218d9e513cce6e15a17fd73f05cbed
Peat <hendy123@gmx.de>
2025-05-13 00:21:15 +0200

    add zap as logger. add configs. add logging stuff.

--- backend/main.go
+++ backend/main.go
@@ -4,8 +4,12 @@ import (
 	"backend/config"
 	"fmt"
 	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"io"
 	"log"
 	"net/http"
+	"os"
+	"time"
 )
 
 type DiscoveredArtist struct {
@@ -18,12 +22,33 @@ type DiscoveredArtistsRequest struct {
 }
 
 func main() {
-	log.Println("loading config")
-
+	log.Println("starting application")
 	cfg := config.LoadConfig("config.yaml")
 
-	log.Println("starting server")
+	var logger *zap.Logger
+	zapEnvironment := *cfg.Logging.Zap
+	if zapEnvironment == "production" {
+		logger, _ = zap.NewProduction()
+	} else {
+		gin.ForceConsoleColor()
+		logger, _ = zap.NewDevelopment()
+	}
+
+	logger.Info("config loaded")
+
+	if cfg.Logging.File != nil {
+		ginLogFile, err := os.OpenFile(*cfg.Logging.File, os.O_APPEND|os.O_CREATE, 0644)
+		if err != nil {
+			logger.Warn("error creating file", zap.String("file", *cfg.Logging.File), zap.Error(err))
+		} else {
+			defer ginLogFile.Close()
+		}
+
+		gin.DefaultWriter = io.MultiWriter(os.Stdout, ginLogFile)
+	}
+
 	r := gin.Default()
+	r.Use(ZapLogger(logger))
 
 	r.GET("/health", getHealthStatus)
 
@@ -31,10 +56,25 @@ func main() {
 	err := r.Run(formattedPort)
 
 	if err != nil {
-		log.Fatalf("failed to start server: %v", err)
+		logger.Fatal("failed to start server: %v", zap.Error(err))
 	}
 }
 
 func getHealthStatus(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{"status": "ok"})
+}
+
+func ZapLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start)
+
+		logger.Info("Request",
+			zap.String("method", c.Request.Method),
+			zap.String("path", c.Request.URL.Path),
+			zap.Int("status", c.Writer.Status()),
+			zap.Duration("duration", duration),
+		)
+	}
 }
