0905c14ae5a040d52571cf729181104318cebd5d
Peat <hendy123@gmx.de>
2025-05-16 23:02:43 +0200

    implement fake endpoints for random data generation

--- backend/mockserver/mockserver.go
+++ backend/mockserver/mockserver.go
@@ -3,8 +3,10 @@ package mockserver
 import (
 	"backend/spotifyapi"
 	"fmt"
+	"github.com/brianvoe/gofakeit/v7"
 	"github.com/gin-gonic/gin"
 	"go.uber.org/zap"
+	"math/rand/v2"
 )
 
 type Server struct {
@@ -17,15 +19,48 @@ func (s *Server) RunSpotifyMockServer() error {
 
 	formattedPort := fmt.Sprintf(":%d", s.Port)
 
-	r.POST("/api/token", func(context *gin.Context) {
-		response := &spotifyapi.ClientCredentials{
-			AccessToken: "MOCK_ACCESS_TOKEN",
-			TokenType:   "Bearer",
-			ExpiresIn:   3600,
-		}
-
-		context.JSON(200, response)
-	})
+	r.POST("/api/token", handlePostToken)
+	r.GET("/api/v1/artists/:id", handleGetArtist)
+	r.GET("/api/v1/artists", handleGetArtists)
 
 	return r.Run(formattedPort)
+}
+
+func handleGetArtist(context *gin.Context) {
+	id := context.Param("id")
+	response := generateRndArtist(id)
+	context.JSON(200, response)
+}
+
+func handlePostToken(context *gin.Context) {
+	response := &spotifyapi.ClientCredentials{
+		AccessToken: gofakeit.Password(true, true, true, false, false, 64),
+		TokenType:   "Bearer",
+		ExpiresIn:   3600,
+	}
+
+	context.JSON(200, response)
+}
+
+func handleGetArtists(context *gin.Context) {
+	ids := context.QueryArray("ids")
+	artists := make([]spotifyapi.Artist, 0)
+	for _, id := range ids {
+		artists = append(artists, generateRndArtist(id))
+	}
+	context.JSON(200, gin.H{"artists": artists})
+}
+
+func generateRndArtist(id string) spotifyapi.Artist {
+	rndGenreCount := rand.IntN(4) + 1
+	rndGenres := make([]string, 0)
+	for i := 0; i < rndGenreCount; i++ {
+		rndGenres = append(rndGenres, gofakeit.SongGenre())
+	}
+	return spotifyapi.Artist{
+		Id:     id,
+		Name:   gofakeit.SongArtist(),
+		Uri:    gofakeit.UUID(),
+		Genres: rndGenres,
+	}
 }
