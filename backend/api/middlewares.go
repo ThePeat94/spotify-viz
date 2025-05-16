55de500273eaa5bcff88b9eba66f9e66f98545ef
Peat <hendy123@gmx.de>
2025-05-17 00:02:17 +0200

    refactor api server into own package

---
+++ backend/api/middlewares.go
@@ -0,0 +1,22 @@
+package api
+
+import (
+	"github.com/gin-gonic/gin"
+	"go.uber.org/zap"
+	"time"
+)
+
+func ZapLogger(logger *zap.Logger) gin.HandlerFunc {
+	return func(c *gin.Context) {
+		start := time.Now()
+		c.Next()
+		duration := time.Since(start)
+
+		logger.Info("Request",
+			zap.String("method", c.Request.Method),
+			zap.String("path", c.Request.URL.Path),
+			zap.Int("status", c.Writer.Status()),
+			zap.Duration("duration", duration),
+		)
+	}
+}
