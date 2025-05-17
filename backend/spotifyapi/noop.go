8b9fae312b04dab72b8016da260e13790da03417
Peat <hendy123@gmx.de>
2025-05-17 03:51:44 +0200

    extend noop client. adjust query param handling.

--- backend/spotifyapi/noop.go
+++ backend/spotifyapi/noop.go
@@ -8,7 +8,6 @@ type NoopClient struct {
 
 func (n *NoopClient) Login() error {
 	n.Logger.Info("Noop: Logging in")
-
 	return nil
 }
 
@@ -19,5 +18,15 @@ func (n *NoopClient) GetArtist(id string) (*Artist, error) {
 
 func (n *NoopClient) GetArtists(ids []string) ([]Artist, error) {
 	n.Logger.Info("Noop: Getting artists")
+	return nil, nil
+}
+
+func (n *NoopClient) GetTrack(id string) (*Track, error) {
+	n.Logger.Info("Noop: Getting track", zap.String("id", id))
+	return nil, nil
+}
+
+func (n *NoopClient) GetTracks(ids []string) ([]Track, error) {
+	n.Logger.Info("Noop: Getting tracks")
 	return nil, nil
 }
