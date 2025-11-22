147052c48d4b990db30c37255fcd395b6383cb9f
Hendrik <dev@hendrikrakemann.de>
2025-11-22 18:14:29 +0100

    style wrapped cards with some random gradients. add wrapped years

--- frontend/src/components/cards/wrapped/MiniWrappedCards.tsx
+++ frontend/src/components/cards/wrapped/MiniWrappedCards.tsx
@@ -2,14 +2,14 @@ import React, { useMemo } from 'react';
 import { Grid2, Stack, Typography } from '@mui/material';
 import { PlaybackData } from 'src/streams/type';
 import { analyzeArtists, analyzeSongs } from 'src/utils/analysis';
-import { ToProcessType, WrappedCardData, WrappedData } from 'src/components/cards/wrapped/type';
+import { ToProcessType, WrappedCardData, WrappedData, WrappedYearCardData } from 'src/components/cards/wrapped/type';
 import MiniWrappedCard from 'src/components/cards/wrapped/MiniWrappedCard';
 
 type Props = {
     rawData: PlaybackData[];
 };
 
-const processRawData = (rawData: PlaybackData[]): WrappedCardData => {
+const processRawData = (rawData: PlaybackData[]): [WrappedCardData, WrappedYearCardData] => {
     const emptyToProcessCluster : ToProcessType = {};
     const clustered = rawData.reduce((acc, curr) => {
         const ts = new Date(curr.ts);
@@ -33,8 +33,8 @@ const processRawData = (rawData: PlaybackData[]): WrappedCardData => {
 
     const analyzedClusters: WrappedCardData = Object.entries(clustered).reduce((accY, [year, data]) => {
         accY[Number(year)] = Object.entries(data).reduce((accM, [month, pbData]) => {
-            const topArtists = analyzeArtists(pbData).sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 5);
-            const topSongs = analyzeSongs(pbData).sort((a, b) => b.msPlayed - a.msPlayed).slice(0, 5);
+            const topArtists = analyzeArtists(pbData).sort((a, b) => b.count - a.count).slice(0, 5);
+            const topSongs = analyzeSongs(pbData).sort((a, b) => b.count - a.count).slice(0, 5);
 
             accM[Number(month)] = {
                 topArtists,
@@ -48,22 +48,38 @@ const processRawData = (rawData: PlaybackData[]): WrappedCardData => {
 
         return accY;
     }, emptyAnalyzedClusters);
-    console.log(analyzedClusters);
-    return analyzedClusters;
+
+    const analyzedYearClusters: WrappedYearCardData = Object.entries(clustered).reduce((accY, [year, data]) => {
+        const allPbData = Object.values(data).flat();
+
+        const topArtists = analyzeArtists(allPbData).sort((a, b) => b.count - a.count).slice(0, 5);
+        const topSongs = analyzeSongs(allPbData).sort((a, b) => b.count - a.count).slice(0, 5);
+
+        accY[Number(year)] = {
+            topArtists,
+            topSongs,
+            totalStreams: allPbData.length,
+            totalPlayedMs: allPbData.reduce((a, b) => a + b.ms_played, 0),
+        };
+
+        return accY;
+    }, {} as WrappedYearCardData);
+
+    return [analyzedClusters, analyzedYearClusters];
 };
 
 /**
  * A component which displays all mini wrap cards
  */
 const MiniWrappedCards: React.FC<Props> = ({ rawData }) => {
-    const processedData = useMemo(() => processRawData(rawData), [rawData]);
+    const [processedMonthData, processedYearData] = useMemo(() => processRawData(rawData), [rawData]);
     return (
         <Stack>
-            <Typography variant={'h4'}>Your Mini Wrappeds</Typography>
+            <Typography variant={'h4'}>Your Monthly Mini Wrappeds</Typography>
             <Grid2 container={true} spacing={2}>
-                {Object.entries(processedData).map(([year, months]) => (
+                {Object.entries(processedMonthData).map(([year, months]) => (
                     Object.entries(months).map(([month, monthsData]) => (
-                        <Grid2 size={6}
+                        <Grid2 size={4}
                             key={`${year}-${month}`}
                         >
                             <MiniWrappedCard
@@ -73,6 +89,18 @@ const MiniWrappedCards: React.FC<Props> = ({ rawData }) => {
                             />
                         </Grid2>
                     ))
+                ))}
+            </Grid2>
+            <Grid2 container={true} spacing={2}>
+                {Object.entries(processedYearData).map(([year, yearData]) => (
+                    <Grid2 size={4}
+                        key={`${year}-yearly`}
+                    >
+                        <MiniWrappedCard
+                            year={Number(year)}
+                            wrappedData={yearData}
+                        />
+                    </Grid2>
                 ))}
             </Grid2>
         </Stack>
