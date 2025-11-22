3a173a0cf095db7d5a93b0ccaaa2bec30f3c41bd
Hendrik <dev@hendrikrakemann.de>
2025-11-22 13:41:22 +0100

    Beautify Data Filter

--- frontend/src/components/DataFilter.tsx
+++ frontend/src/components/DataFilter.tsx
@@ -1,6 +1,16 @@
 import React, { useMemo, useState } from 'react';
 import { DataFilterType } from 'src/filter/type';
-import { Button, FormControl, Grid2, InputLabel, MenuItem, Select, Slider, Stack, Typography } from '@mui/material';
+import {
+    Button, Card, CardContent, CardHeader,
+    FormControl,
+    Grid2,
+    InputLabel,
+    MenuItem,
+    Select,
+    Slider,
+    Stack,
+    Typography
+} from '@mui/material';
 import { DatePicker } from '@mui/x-date-pickers';
 import moment, { Moment } from 'moment/moment';
 import { generateYearSelections } from 'src/data/analysis';
@@ -115,107 +125,110 @@ const DataFilter: React.FC<DataFilterProps> = (props) => {
 
 
     return (
-        <Grid2 container={true} alignItems={'center'} spacing={2}>
-            <Grid2 size={12}>
-                <Typography variant={'h4'} pt={2}>Filter</Typography>
-            </Grid2>
-            <Grid2 size={4} p={2}>
-                <Stack>
-                    <Typography variant={'body2'}>Min. Duration</Typography>
-                    <Slider
-                        min={0}
-                        max={60_000}
-                        step={1000}
-                        marks={true}
-                        onDragStart={() => console.log('dragging started')}
-                        onDrag={() => console.log('dragging')}
-                        onDragEnd={() => console.log('dragging ended')}
-                        onDragStartCapture={() => console.log('on drag start capture')}
-                        onChange={(_, v) => setTmpDuration(v as number)}
-                        onChangeCommitted={(_, v) => handleMinDurationChange(v as number)}
-                        value={tmpDuration}
-                        valueLabelDisplay={'auto'}
-                        valueLabelFormat={(v => `${v/1000}s`)}
-                    />
-                    <Typography variant={'caption'}>{minDuration/1000}s</Typography>
-                </Stack>
-            </Grid2>
-            <Grid2 size={2}>
-                <DatePicker
-                    maxDate={toDate ?? undefined}
-                    label={'From'}
-                    value={fromDate}
-                    onChange={v => handleFromDateChange(v)}
-                    format={'DD.MM.YYYY'}
-                    sx={{ width: '100%' }}
+        <Card>
+            <CardHeader title={<Typography variant={'h4'}>Filter</Typography>}/>
+            <CardContent>
+                <Grid2 container={true} alignItems={'center'} spacing={2}>
+                    <Grid2 size={4} p={2}>
+                        <Stack>
+                            <Typography variant={'body2'}>Min. Duration</Typography>
+                            <Slider
+                                min={0}
+                                max={60_000}
+                                step={1000}
+                                marks={true}
+                                onDragStart={() => console.log('dragging started')}
+                                onDrag={() => console.log('dragging')}
+                                onDragEnd={() => console.log('dragging ended')}
+                                onDragStartCapture={() => console.log('on drag start capture')}
+                                onChange={(_, v) => setTmpDuration(v as number)}
+                                onChangeCommitted={(_, v) => handleMinDurationChange(v as number)}
+                                value={tmpDuration}
+                                valueLabelDisplay={'auto'}
+                                valueLabelFormat={(v => `${v/1000}s`)}
+                            />
+                            <Typography variant={'caption'}>{minDuration/1000}s</Typography>
+                        </Stack>
+                    </Grid2>
+                    <Grid2 size={2}>
+                        <DatePicker
+                            maxDate={toDate ?? undefined}
+                            label={'From'}
+                            value={fromDate}
+                            onChange={v => handleFromDateChange(v)}
+                            format={'DD.MM.YYYY'}
+                            sx={{ width: '100%' }}
 
-                />
-            </Grid2>
-            <Grid2 size={2}>
-                <DatePicker
-                    minDate={fromDate ?? undefined}
-                    label={'To'}
-                    value={toDate}
-                    onChange={v => handleToDateChange(v)}
-                    format={'DD.MM.YYYY'}
-                    sx={{ width: '100%' }}
-                />
-            </Grid2>
-            <Grid2 size={1}>
-                <Button
-                    variant={'outlined'}
-                    onClick={() => {
-                        onChange({
-                            ...value,
-                            from: undefined,
-                            to: undefined,
-                        });
-                    }}
-                >
-                    Reset
-                </Button>
-            </Grid2>
-            <Grid2 size={3}/>
-            <Grid2 size={12}>
-                <Typography variant={'h6'}>
-                    Filter Presets
-                </Typography>
-            </Grid2>
-            <Grid2 size={2}>
-                <FormControl fullWidth={true}>
-                    <InputLabel id={'preset-year-select-label'}>Year</InputLabel>
-                    <Select
-                        labelId={'preset-year-select-label'}
-                        label={'Year'}
-                        value={presetFilter.year}
-                        onChange={e => handlePresetYearChange(e.target.value as number)}
-                        fullWidth={true}
-                        autoWidth={false}
-                        disabled={!latestYear && !earliestYear}
-                    >
-                        <MenuItem value={undefined}>None</MenuItem>
-                        {earliestYear && latestYear && generateYearSelections(earliestYear, latestYear)}
-                    </Select>
-                </FormControl>
-            </Grid2>
-            <Grid2 size={2}>
-                <FormControl fullWidth={true}>
-                    <InputLabel id={'filter-mode-select-label'}>Mode</InputLabel>
-                    <Select
-                        labelId={'filter-mode-select-label'}
-                        label={'Mode'}
-                        value={presetFilter.mode}
-                        onChange={e => handlePresetModeChange(e.target.value as FilterModeType)}
-                        fullWidth={true}
-                        autoWidth={false}
-                        disabled={!latestYear && !earliestYear}
-                    >
-                        <MenuItem value={'allYear'}>All Year</MenuItem>
-                        <MenuItem value={'wrapped'}>Wrapped</MenuItem>
-                    </Select>
-                </FormControl>
-            </Grid2>
-        </Grid2>
+                        />
+                    </Grid2>
+                    <Grid2 size={2}>
+                        <DatePicker
+                            minDate={fromDate ?? undefined}
+                            label={'To'}
+                            value={toDate}
+                            onChange={v => handleToDateChange(v)}
+                            format={'DD.MM.YYYY'}
+                            sx={{ width: '100%' }}
+                        />
+                    </Grid2>
+                    <Grid2 size={1}>
+                        <Button
+                            variant={'outlined'}
+                            onClick={() => {
+                                onChange({
+                                    ...value,
+                                    from: undefined,
+                                    to: undefined,
+                                });
+                            }}
+                        >
+                            Reset
+                        </Button>
+                    </Grid2>
+                    <Grid2 size={3}/>
+                    <Grid2 size={12}>
+                        <Typography variant={'h6'}>
+                            Filter Presets
+                        </Typography>
+                    </Grid2>
+                    <Grid2 size={2}>
+                        <FormControl fullWidth={true}>
+                            <InputLabel id={'preset-year-select-label'}>Year</InputLabel>
+                            <Select
+                                labelId={'preset-year-select-label'}
+                                label={'Year'}
+                                value={presetFilter.year}
+                                onChange={e => handlePresetYearChange(e.target.value as number)}
+                                fullWidth={true}
+                                autoWidth={false}
+                                disabled={!latestYear && !earliestYear}
+                            >
+                                <MenuItem value={undefined}>None</MenuItem>
+                                {earliestYear && latestYear && generateYearSelections(earliestYear, latestYear)}
+                            </Select>
+                        </FormControl>
+                    </Grid2>
+                    <Grid2 size={2}>
+                        <FormControl fullWidth={true}>
+                            <InputLabel id={'filter-mode-select-label'}>Mode</InputLabel>
+                            <Select
+                                labelId={'filter-mode-select-label'}
+                                label={'Mode'}
+                                value={presetFilter.mode}
+                                onChange={e => handlePresetModeChange(e.target.value as FilterModeType)}
+                                fullWidth={true}
+                                autoWidth={false}
+                                disabled={!latestYear && !earliestYear}
+                            >
+                                <MenuItem value={'allYear'}>All Year</MenuItem>
+                                <MenuItem value={'wrapped'}>Wrapped</MenuItem>
+                            </Select>
+                        </FormControl>
+                    </Grid2>
+                </Grid2>
+
+            </CardContent>
+        </Card>
     );
 };
 
