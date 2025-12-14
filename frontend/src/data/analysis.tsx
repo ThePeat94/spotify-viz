4b6d5c88853f142948dcfb28e4dfc5f7df4f93a0
Peat <hendy123@gmx.de>
2025-12-14 17:57:03 +0100

    implement optimized algorithm to analyze playback data

--- frontend/src/data/analysis.tsx
+++ frontend/src/data/analysis.tsx
@@ -2,6 +2,30 @@ import { PlaybackData } from 'src/streams/type';
 import { MenuItem } from '@mui/material';
 import React from 'react';
 
+type AnalysisResult = {
+    uniqueArtistCount: number;
+    uniqueSongCount: number;
+}
+
+export const calculateUniqueArtistAndSongCount = (playbackData: PlaybackData[]): AnalysisResult => {
+    const foundArtists = new Set<string>();
+    const uniqueSongs  = new Set<string>();
+
+    playbackData.forEach(pb => {
+        if (pb.master_metadata_album_artist_name !== null) {
+            foundArtists.add(pb.master_metadata_album_artist_name);
+        }
+
+        uniqueSongs.add(pb.spotify_track_uri);
+    });
+
+
+    return {
+        uniqueArtistCount: foundArtists.size,
+        uniqueSongCount: uniqueSongs.size,
+    };
+};
+
 export const calculateUniqueArtistCount = (playbackData: PlaybackData[]): number => {
     const foundArtists : string[] = [];
     return playbackData
